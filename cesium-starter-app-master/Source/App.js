a1cfbf0e53ac214298b62a6f6624f97d27c76777
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-09 11:23:25 +0900

    ラインを追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -21,3 +21,16 @@ point2.add({
             color : Cesium.Color.BLUE
     }
 });
+
+//ラインを追加
+var line1 = viewer.entities;
+
+line1.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+                                                        -125, 35]),
+        width : 5,
+        material : Cesium.Color.RED
+    }
+});
