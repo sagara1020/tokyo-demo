df6f6c376a206d52fe7d55a0c4719fe649ce76b0
sagara1020 <sagara1020@gmail.com>
2015-10-23 16:06:10 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -6,4 +6,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 //kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/test.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/test.kmz'));
