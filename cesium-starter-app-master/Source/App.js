69a9f5944b5837afeec3dee7694fbb3e08d1c4cf
sagara1020 <sagara1020@gmail.com>
2016-02-18 12:37:03 +0900

    move to fukuchiyama

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,9 +1,39 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
-
-
-
-//視点の変更
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
-
-//kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/test.kml'));
+var czml = [
+  {
+    "id" : "document",
+    "name" : "fukuchiyama",
+    "version" : "1.0"
+  }, {
+    "id" : "1",
+    "name" : "Kさん",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.3
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1320894411087,35.28799163469748,200
+      ]
+    }
+  }
+];
+viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/demo/cesium-starter-app-master/Source/water_area.kml'));
